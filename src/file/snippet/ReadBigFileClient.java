41cb7b1ab36dd6b0c3eac352eae39fbabce4de6f
fredzhang <wzhangwor@163.com>
2015-04-05 19:12:05 +0800

    2g的数据 找某个字符多少个

--- src/file/snippet/ReadBigFileClient.java
+++ src/file/snippet/ReadBigFileClient.java
@@ -4,11 +4,14 @@ import java.io.File;
 
 public class ReadBigFileClient {
 	public static void main(String[] args) {
-		File file = new File("D://document.txt");  
-		BigFileHandler searchThread = new BigFileHandler(file,"呵呵",0,Integer.MAX_VALUE);                
-		Thread thread=new Thread(searchThread);
+		File file = new File("D://document.txt");
+		BigFileHandler searchThread = new BigFileHandler(file, "aa", 0,
+				Integer.MAX_VALUE);
+		Thread thread = new Thread(searchThread);
 		thread.start();
-	/*	String str="我去123";
-	    System.out.print(str.indexOf("我去"));*/
+	
+		/*
+		 * String str="我去123"; System.out.print(str.indexOf("我去"));
+		 */
 	}
 }
